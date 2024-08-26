50ace589f8ce5ace7b0d71f4e40ca561170ef7d8
Venkat <lvenkat082624@gmail.com>
2024-08-26 14:43:15 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print("I am trying to print a new Hellhelloo world here")
+print("I am trying to print a new palakura world here")
 print("Hello, World!") - A basic print statement with a string message.
 print("08, 26, 24") 
 print("Message", file=open("output.txt", "w")) 
