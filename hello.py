87a96e69c52f30bd0f49825aefef70b3f0b38870
Venkat <dvlprasad123@gmail.com>
2024-08-26 14:40:02 -0500

    Updated hello and changes

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("I am trying to print a new Hello world here")
+print("Hello, World!") - A basic print statement with a string message.
+print("08, 26, 24") 
+print("Message", file=open("output.txt", "w")) 
+print("Hello Geeks!!", file=dummy_file) - Uses a dummy file object created with the io module to demonstrate printing to a file.
